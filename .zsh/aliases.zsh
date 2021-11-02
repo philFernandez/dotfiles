d6abf50bb203d73ff7cc35dc3917724fce0033ad
philFernandez <norcal618@gmail.com>
2021-11-01 23:47:24 -0700

    move ZDOTDIR to ~/.zsh

--- .zsh/aliases.zsh
+++ .zsh/aliases.zsh
@@ -9,9 +9,15 @@ alias cl='clear;la'
 alias cll='clear;ll'
 alias ev='vim ~/.vimrc'
 alias ep='vim ~/.zsh/aliases.zsh'
+alias el='vim ~/.zsh/.zlogin'
 alias rc='vim ~/.zshrc'
 alias cpdir="pwd | tr -d '\n' | pbcopy"
 # Dotfiles Version Control {{{1
 alias config='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
 alias cst='config status'
 alias cpush='config push -u origin main'
+alias cdl='config diff'
+# Homebrew {{{1
+alias clean='brew cleanup --prune=all -s'
+alias update='brew -v update'
+alias upgrade='brew upgrade'
