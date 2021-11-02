99b062a8bb484f067055bbbf0348eaa73378c6d7
philFernandez <norcal618@gmail.com>
2021-11-02 15:11:13 -0700

    break up aliases into subgroups

--- .zsh/aliases.zsh
+++ .zsh/aliases.zsh
@@ -1,6 +1,15 @@
+# Aliases {{{1
+# Misc {{{2
 alias reload='exec zsh'
 alias q='exit'
 alias ppath='print -l $path'
+alias cpdir="pwd | tr -d '\n' | pbcopy"
+# Config File Shortcuts {{{2
+alias ev='vim ~/.vimrc'
+alias ep='vim ~/.zsh/aliases.zsh'
+alias el='vim ~/.zsh/.zlogin'
+alias rc='vim ~/.zsh/.zshrc'
+# Navigating and Displaying {{{2
 alias cls=clear
 alias ls='lsd --group-dirs first'
 alias la='ls -A'
@@ -9,13 +18,8 @@ alias l='lsd --blocks date,name,size --date relative -tA'
 alias cl='clear;la'
 alias cll='clear;ll'
 alias bk='cd ..'
-alias ev='vim ~/.vimrc'
-alias ep='vim ~/.zsh/aliases.zsh'
-alias el='vim ~/.zsh/.zlogin'
-alias rc='vim ~/.zsh/.zshrc'
-alias cpdir="pwd | tr -d '\n' | pbcopy"
 alias fl='fd -HI -d1'
-# Dotfiles Version Control {{{1
+# Dotfiles Version Control {{{2
 alias config='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
 alias cst='config status'
 alias cpush='config push -u origin main'
@@ -23,7 +27,15 @@ alias cdl='config diff'
 alias clcp='config log -p'
 alias ccam='config commit -a -m'
 alias clo='config log --oneline --all --decorate --graph'
-# Homebrew {{{1
+# Homebrew {{{2
 alias clean='brew cleanup --prune=all -s'
 alias update='brew -v update'
 alias upgrade='brew upgrade'
+# Global Aliases {{{1
+# Goto Shortcuts {{{2
+alias -g SOU="~/Source_Code"
+alias -g DOC="~/Documents"
+alias -g DOW="~/Downloads"
+alias -g BIN="~/.bin"
+# Modeline {{{1
+# vim:foldenable foldmethod=marker foldcolumn=1 foldlevel=0
