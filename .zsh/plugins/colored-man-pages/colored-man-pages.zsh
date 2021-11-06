59bb1dfd28589dee2411c916462aba7093fcf5ec
philFernandez <norcal618@gmail.com>
2021-11-06 15:02:09 -0700

    put cursor things in theme.conf

--- .zsh/plugins/colored-man-pages/colored-man-pages.zsh
+++ .zsh/plugins/colored-man-pages/colored-man-pages.zsh
@@ -5,7 +5,7 @@ function colored() {
     LESS_TERMCAP_md=$(printf "\e[1;32m") \
     LESS_TERMCAP_me=$(printf "\e[0m") \
     LESS_TERMCAP_se=$(printf "\e[0m") \
-    LESS_TERMCAP_so=$(printf "\e[01;33m") \
+    LESS_TERMCAP_so=$(printf "\e[30;43m") \
     LESS_TERMCAP_ue=$(printf "\e[0m") \
     LESS_TERMCAP_us=$(printf "\e[1;4;31m") \
     PAGER="${commands[less]:-$PAGER}" \
